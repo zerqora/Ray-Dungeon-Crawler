366df1ae7ebc357dc712021487caa6c3e73bbe28
zerqora <angelduong0@gmail.com>
2025-08-14 18:55:17 -0500

    door interact area almost done

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,3 +1,1 @@
 class_name Door extends Node2D
-
-var next_room
--- scripts/interactable_area.gd
+++ scripts/interactable_area.gd
@@ -1,13 +1,27 @@
 class_name InteractArea extends Area2D
 
-signal entered_area()
-signal exited_area()
+signal entered_area
+signal exited_area
 
+@onready var interact_label : Label = $"Press E To Interact"
+
+func _ready() -> void:
+	entered_area.connect(show_interactable)
+	exited_area.connect(hide_interactable)
+	interact_label.hide()
+	
+	
 #TODO: make this return an entity
-func on_entered_area() -> void:
+func _on_entered_area(area: Area2D) -> void:
 	entered_area.emit()
 
-func on_exited_area() -> void:
+func _on_exited_area(area: Area2D) -> void:
 	exited_area.emit()
+	
+func show_interactable() -> void:
+	interact_label.show()
+
+func hide_interactable() -> void:
+	interact_label.hide()
 	
 	
