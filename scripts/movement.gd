8b5d8710893eb2f4630e45658992f8aefe2100d7
zerqora <angelduong0@gmail.com>
2025-08-14 17:42:58 -0500

    character movement done

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -5,9 +5,14 @@ enum DIRECTION {
 	RIGHT = 1,
 	LEFT = -1
 }
-var speed = 5000
-var direction = DIRECTION.NONE
-var gravity = 5000
+var speed: int = 5000
+var direction: int = DIRECTION.NONE
+var gravity: int = 5000
+
+@onready var animation = $Sprite2D
+
+func _ready() -> void:
+	animation.play("idle")
 
 func _physics_process(delta: float) -> void:
 	_handle_input()
@@ -16,12 +21,19 @@ func _physics_process(delta: float) -> void:
 		velocity.y = gravity * delta
 	move_and_slide()
 
+# TODO: Implement State Machine
 func _handle_input() -> void:
-	if Input.is_action_just_pressed("LEFT"):
-		direction = DIRECTION.LEFT
-	if Input.is_action_just_pressed("RIGHT"):
-		direction = DIRECTION.RIGHT
-	if Input.is_action_just_released("LEFT") or Input.is_action_just_released("RIGHT"):
+	var stopped_moving: bool = Input.is_action_just_released("LEFT") or Input.is_action_just_released("RIGHT")
+	var cancelled_out_direction : bool = Input.is_action_pressed("LEFT") and Input.is_action_pressed("RIGHT")
+	
+	if stopped_moving or cancelled_out_direction:
 		direction = DIRECTION.NONE
+	elif Input.is_action_pressed("LEFT"):
+		direction = DIRECTION.LEFT
+		animation.flip_h = false
+	elif Input.is_action_pressed("RIGHT"):
+		direction = DIRECTION.RIGHT
+		animation.flip_h = true
+	
 		
 	
