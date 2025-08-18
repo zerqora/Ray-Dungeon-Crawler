faaf92a2420eadab3036edf06bdb57636806c0fa
zerqora <angelduong0@gmail.com>
2025-08-18 11:45:08 -0500

    get started on basic enemy

--- scripts/entity.gd
+++ scripts/entity.gd
@@ -1,6 +1,10 @@
 class_name Entity extends CharacterBody2D
 
 @export var entity_stats : EntityStats
+@export var hitbox : Area2D
+@export var damage : Area2D
+
+var speed = entity_stats.speed
 var current_hp : int = entity_stats.health
 
 func take_damage(amount: float) -> void:
@@ -8,3 +12,7 @@ func take_damage(amount: float) -> void:
 
 func take_knockback(force : float) -> void:
 	pass
+
+func _deal_damage(who: Player) -> void:
+	pass
+	
